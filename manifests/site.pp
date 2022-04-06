198b6520bd725a529e8f3cf28184c3675c4c917d
dnemtsanov <dnemtsanov@gmail.com>
2022-04-05 23:34:53 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,13 +7,14 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content = $fqdn,
+    }
 }
 node /^web/ { 
   include role::app_server
 }
 node /^db/ {
   include role::db_server
-}
-class { 'docker':
-  use_upstream_package_source => false,
 }
